44b5491a57633ba8a13d86ce701d5a8435ad751b
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-05 10:32:05 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,4 +13,7 @@ sed -i "s/YOUR_PUBLIC_KEY/${VITE_EMAILJS_KEY}/g" dist/js/app.js
 sed -i "s/YOUR_SERVICE_ID/${VITE_EMAILJS_SERVICE}/g" dist/js/app.js
 sed -i "s/YOUR_TEMPLATE_ID/${VITE_EMAILJS_TEMPLATE}/g" dist/js/app.js
 
+# Transpile JSX to plain JavaScript using esbuild
+npx --yes esbuild dist/js/app.js --bundle --outfile=dist/js/app.js --loader:.js=jsx
+
 echo "Build completed. Files are in the dist directory."
